60ee275695998dbc39c3990083c922c005a13c26
brindab50@gmail.com behal <brindab50@gmail.com>
2018-08-29 20:17:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function happyHolidays() {
   return("Happy holidays!")
 }
-happyHolidays()
-function happyHolidaysTo(name) {
-return(`Happy holidays,${name}!`)
-}
-happyHolidaysTo("Brinda")
 
-function happyHolidaysTo(name, holiday) {
-  return "happy ${Thanksgiving}, ${Brinda Behal}!"
+function happyHolidaysTo(name) {
+return(`Happy holidays, ${name}!`)
 }
+
+
